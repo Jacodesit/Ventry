9bc32ad630ed4d0edaa2079a6f70eaf7db48b283
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2026-03-15 21:36:40 +0800

    Make the posting working

--- resources/js/pages/homepage/components/form/rant-form.tsx
+++ resources/js/pages/homepage/components/form/rant-form.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "@inertiajs/react"
 import { Button } from "@/components/ui/button"
 // import { Checkbox } from "@/components/ui/checkbox"
 import {
@@ -19,13 +20,37 @@ type pageProps = {
 }
 
 export default function RantForm({onClose, emotions}:pageProps) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        nickname: '',
+        emoji_id: '',
+        message: ''
+    })
+
+    const submit = (e: React.FormEvent) => {
+        e.preventDefault()
+        post('/posts', {
+            onSuccess: () => {
+                reset()
+                onClose();
+            }
+        })
+
+    }
+
     return (
         <div className="w-full max-w-2xl">
-            <form>
+            <form onSubmit={submit}>
                 <FieldGroup>
                     <Field>
-                        <FieldLabel htmlFor="nickname">Nickname</FieldLabel>
-                        <Input id="nickname" autoComplete="on" placeholder="Hacob" />
+                        <FieldLabel htmlFor="nickname">Nickname <span className="text-blue-500">(Optional)</span></FieldLabel>
+                        <Input
+                            value={data.nickname}
+                            onChange={(e) => setData('nickname', e.target.value)}
+                            id="nickname"
+                            autoComplete="on"
+                            placeholder="Hacob"
+                        />
+                        {errors.nickname && <p className="errors text-xs text-destructive">{errors.nickname}</p>}
                     </Field>
 
                     <Field>
@@ -33,32 +58,43 @@ export default function RantForm({onClose, emotions}:pageProps) {
                         <div className="flex flex-wrap gap-2">
                             {emotions.map(emotion => (
                                 <button
+                                    value={data.emoji_id}
+                                    onChange={(e) => setData('emoji_id', e.target.value)}
                                     key={emotion.id}
                                     type="button"
-                                    className="flex border px-3 py-1 rounded-md gap-2"
+                                    className="flex border px-3 py-1 rounded-md gap-2 cursor-pointer transition-all duration-300 hover:bg-accent-foreground hover:text-muted"
                                 >
                                     {emotion.emoji}
                                     {emotion.name}
                                 </button>
                             ))}
                         </div>
-
+                        {errors.emoji_id && <p className="errors text-xs text-destructive">{errors.emoji_id}</p>}
                     </Field>
 
                     <Field>
                         <FieldLabel htmlFor="message">Message</FieldLabel>
                         <Textarea
-                            id="checkout-7j9-optional-comments"
-                            placeholder="Add any additional comments"
+                            value={data.message}
+                            onChange={(e) => setData('message', e.target.value)}
+                            id="message"
+                            placeholder="What's on your mind?"
+                            rows={20}
                             className="resize-none"
                         />
+                        {errors.message && <p className="errors text-xs text-destructive">{errors.message}</p>}
                     </Field>
 
 
                     <Field className="flex justify-end" orientation="horizontal">
-                        <Button type="submit">Submit</Button>
+                        <Button
+                            disabled={processing}
+                            type="submit"
+                        >
+                            {processing ? 'Posting...' : 'Post'}
+                        </Button>
                         <Button variant="outline" type="button" onClick={onClose}>
-                        Cancel
+                            Cancel
                         </Button>
                     </Field>
                 </FieldGroup>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\EmotionController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', fn() => Inertia::render('welcome'))->name('index');
 Route::get('/wall', [EmotionController::class, 'index'])->name('wall');
+Route::resource('posts', PostController::class)->except('index');
 
 require __DIR__.'/settings.php';
